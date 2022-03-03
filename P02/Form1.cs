b10d3bcb4ba266cd4b13cebff69536158baf92a1
Filip Dusil <dusilf.05@spst.eu>
2022-03-03 11:35:18 +0100

    2-priklad

---
+++ P02/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            Random rnd = new Random();
+            int[] pole = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                pole[i] = rnd.Next(1, 11);
+                listBox1.Items.Add(pole[i].ToString());
+            }
+            if(radioButton1.Checked)
+            {
+
+                Array.Sort(pole);
+                int max = pole.Max();
+                int min = pole.Min();
+                int prvnimax = Array.LastIndexOf(pole, max);
+                int poslednimin = Array.LastIndexOf(pole, min);
+                int dmin = pole[poslednimin + 1];
+                int dmax = pole[prvnimax - 1];
+
+                MessageBox.Show(" druhý max je:" + dmax + "druhy min je" + dmin);
+                foreach (int i in pole)
+                {
+                    listBox2.Items.Add(i);
+                }
+
+            }
+            if (radioButton2.Checked)
+            {
+
+                Array.Sort(pole);
+                Array.Reverse(pole);
+
+                int max = pole.Max();
+                int min = pole.Min();
+                int prvnimax = Array.LastIndexOf(pole, max);
+                int poslednimin = Array.LastIndexOf(pole, min);
+                int dmin = pole[poslednimin - 1];
+                int dmax = pole[prvnimax + 1];
+
+                MessageBox.Show(" druhý max je:" + dmax + "druhy min je" + dmin);
+                foreach (int i in pole)
+                {
+                    listBox2.Items.Add(i);
+                }
+                
+            }
+        }
+    }
+}
